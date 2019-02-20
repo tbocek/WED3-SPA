ce3e4a17ad2532da2dacfe883ab2d16bfb810460
Thomas Bocek <tom@tomp2p.net>
2019-02-20 10:51:47 +0100

    added spa example

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"strconv"
 	//https://godoc.org/github.com/julienschmidt/httprouter, https://github.com/julienschmidt/httprouter
 	"github.com/julienschmidt/httprouter"
 	"html/template"
@@ -12,21 +14,34 @@ type Model struct {
 	Name string
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+var counter = 0
+
+func nospa(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
 	template, err := template.ParseFiles("index.gtpl")
 	if err != nil {
 		log.Fatal(err)
 	}
-	model := Model{Name: ps.ByName("name")}
+	model := Model{Name: ps.ByName("name") + ", counter=" + strconv.Itoa(counter)}
 	err = template.Execute(w, &model)
 	if err != nil {
 		log.Fatal(err)
 	}
+	counter++
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func spa(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
+	json.NewEncoder(w).Encode(Model{Name: ps.ByName("name")})
 }
 
 func main() {
 	log.Print("Starting server...")
 	router := httprouter.New()
-	router.GET("/nospa/:name", helloWorld)
+	router.GET("/nospa/:name", nospa)
+	router.GET("/spa/:name", spa)
+	router.GET("/", index)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
